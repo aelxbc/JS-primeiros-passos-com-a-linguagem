988f29a7d3e140b2e3b2d7b27bc4263748f5ed64
Alex Corrêa <alexxx.nada@gmail.com>
2021-08-20 17:48:36 -0300

    Aula 01

--- 01-meuPrimeiroPrograma.js
+++ 01-meuPrimeiroPrograma.js
@@ -1,2 +1,14 @@
-console.log("Meu primeiro programa JS")
+console.log("Meu primeiro programa JS: trabalhando com variáveis")
+
+const idade = 25 
+
+console.log(idade)
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade*2)
+console.log(idade/2)
+
+const idadeSomada = idade + 2
+console.log(idadeSomada)
+
 
